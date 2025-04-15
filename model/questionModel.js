70efa1bc66f69c6057571bd65c542d8f24e3eac7
ItsmeToney <tonimanthara@gmail.com>
2025-04-15 23:28:36 +0530

    changed model

--- model/questionModel.js
+++ model/questionModel.js
@@ -14,6 +14,10 @@ const testCaseSchema = new mongoose.Schema({
 const questionSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  functionDeclaration: {
+    type: String,
+    required: true,
+  },
   language: {
     type: String,
     required: true,
